c6276b54fc42bd51abca91b54dedba9c7c94352a
ntamasM <manolis.ntamadakis@gmail.com>
2024-10-17 16:21:47 +0300

    test again

--- admin/admin-menu.php
+++ admin/admin-menu.php
@@ -20,7 +20,10 @@ function ntamas_cursor_settings_page_with_image()
 ?>
     <div class="ccf-header">
         <div class="logo">
-            <a href="https://ntamadakis.gr"><img src="<?php echo plugin_dir_url(__DIR__) ?>assets/media/Logo-CCF-by-Ntamas.png" alt="CCFLogo" style="vertical-align: middle; margin-right: 10px;"></a>
+
+            <a href="https://ntamadakis.gr"></a>
+            <img src="<?php echo plugin_dir_url(__DIR__) ?>assets/media/Logo-CCF-by-Ntamas.svg" alt="CCFLogoSVG" style="vertical-align: middle; margin-right: 10px;">
+            </a>
         </div>
         <div class="title">
             <h1>Custom Cursor Follower by Ntamas</h1>

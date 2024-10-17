a981132b3ef436ab792173d12b83debd1404d810
ntamasM <manolis.ntamadakis@gmail.com>
2024-10-17 11:47:41 +0300

    Remove color

--- custom-cursor-follower-ntamas.php
+++ custom-cursor-follower-ntamas.php
@@ -7,7 +7,8 @@ Author: Ntamas
 */
 
 // Enqueue the necessary CSS and JS files
-function ntamas_cursor_follower_enqueue_assets() {
+function ntamas_cursor_follower_enqueue_assets()
+{
     wp_enqueue_style('ntamas-cursor-style', plugin_dir_url(__FILE__) . 'assets/css/styles.css?v0.0.14');
     wp_enqueue_script('ntamas-cursor-script', plugin_dir_url(__FILE__) . 'js/script.js?v0.0.8', array('jquery'), null, true);
 
@@ -21,13 +22,15 @@ function ntamas_cursor_follower_enqueue_assets() {
 }
 add_action('wp_enqueue_scripts', 'ntamas_cursor_follower_enqueue_assets');
 
-function ntamas_enqueue_font_awesome() {
+function ntamas_enqueue_font_awesome()
+{
     wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css');
 }
 add_action('wp_enqueue_scripts', 'ntamas_enqueue_font_awesome');
 
 // Create admin menu
-function ntamas_cursor_follower_menu() {
+function ntamas_cursor_follower_menu()
+{
     add_menu_page(
         'Custom Cursor Follower',  // Page title
         'Cursor Follower',         // Menu title
@@ -41,8 +44,9 @@ function ntamas_cursor_follower_menu() {
 add_action('admin_menu', 'ntamas_cursor_follower_menu');
 
 // Settings page callback
-function ntamas_cursor_settings_page() {
-    ?>
+function ntamas_cursor_settings_page()
+{
+?>
     <div class="wrap">
         <h1>Custom Cursor Follower Settings</h1>
         <form method="post" action="options.php">
@@ -53,42 +57,39 @@ function ntamas_cursor_settings_page() {
             ?>
         </form>
     </div>
-    <?php
+<?php
 }
 
 // Register settings
-function ntamas_cursor_register_settings() {
+function ntamas_cursor_register_settings()
+{
     register_setting('ntamas_cursor_settings_group', 'ntamas_cursor_icon');
-    register_setting('ntamas_cursor_settings_group', 'ntamas_cursor_color');
     register_setting('ntamas_cursor_settings_group', 'ntamas_cursor_size');
     register_setting('ntamas_cursor_settings_group', 'ntamas_cursor_speed');
 
     add_settings_section('ntamas_cursor_main_section', 'Cursor Settings', null, 'ntamas-cursor-settings');
 
     add_settings_field('ntamas_cursor_icon', 'Cursor Icon', 'ntamas_cursor_icon_field', 'ntamas-cursor-settings', 'ntamas_cursor_main_section');
-    add_settings_field('ntamas_cursor_color', 'Icon Color', 'ntamas_cursor_color_field', 'ntamas-cursor-settings', 'ntamas_cursor_main_section');
     add_settings_field('ntamas_cursor_size', 'Icon Size', 'ntamas_cursor_size_field', 'ntamas-cursor-settings', 'ntamas_cursor_main_section');
     add_settings_field('ntamas_cursor_speed', 'Follow Speed', 'ntamas_cursor_speed_field', 'ntamas-cursor-settings', 'ntamas_cursor_main_section');
 }
 add_action('admin_init', 'ntamas_cursor_register_settings');
 
 // Fields for settings page
-function ntamas_cursor_icon_field() {
+function ntamas_cursor_icon_field()
+{
     $icon = get_option('ntamas_cursor_icon');
     echo "<input type='text' name='ntamas_cursor_icon' value='" . esc_attr($icon) . "' placeholder='e.g., fa fa-star'/>";
 }
 
-function ntamas_cursor_color_field() {
-    $color = get_option('ntamas_cursor_color');
-    echo "<input type='color' name='ntamas_cursor_color' value='" . esc_attr($color) . "'/>";
-}
-
-function ntamas_cursor_size_field() {
+function ntamas_cursor_size_field()
+{
     $size = get_option('ntamas_cursor_size');
     echo "<input type='number' name='ntamas_cursor_size' value='" . esc_attr($size) . "' min='1' max='100'/>";
 }
 
-function ntamas_cursor_speed_field() {
+function ntamas_cursor_speed_field()
+{
     $speed = get_option('ntamas_cursor_speed');
     echo "<input type='number' name='ntamas_cursor_speed' value='" . esc_attr($speed) . "' min='1' max='100'/>";
 }

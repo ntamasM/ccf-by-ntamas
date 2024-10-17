e6534489ebc3d7a68ab73754dc8fa3dc85e4fa48
ntamasM <manolis.ntamadakis@gmail.com>
2024-10-17 12:59:45 +0300

    Fix bug

--- custom-cursor-follower-ntamas.php
+++ custom-cursor-follower-ntamas.php
@@ -43,6 +43,7 @@ function ntamas_cursor_follower_menu()
 {
     add_menu_page(
         'Custom Cursor Follower by Ntamas',  // Page title
+        'Cursor Follower',         // Menu title
         'manage_options',          // Capability
         'ntamas-cursor-settings',  // Menu slug
         'ntamas_cursor_settings_page',  // Callback function

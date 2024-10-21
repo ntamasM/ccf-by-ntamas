6f75ac8e024b4d904bf3184c79d77d3232be422e
ntamasM <manolis.ntamadakis@gmail.com>
2024-10-21 15:28:39 +0300

    test for updater

--- custom-cursor-follower-ntamas.php
+++ custom-cursor-follower-ntamas.php
@@ -3,7 +3,7 @@
 Plugin Name: Custom Cursor Follower by Ntamas
 Plugin URI: https://github.com/ntamasM/CCF-by-Ntamas
 Description: A Wordpress plugin to add an icon that will follow your cursor
-Version: 1.0.3
+Version: 1.0.2
 Author: Ntamas
 Author URI: https://ntamadakis.gr
 License: GPL2
